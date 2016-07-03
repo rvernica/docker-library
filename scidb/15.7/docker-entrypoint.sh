0868819e257c3a753b96d74aaaefa5d8ac4fef1d
Rares Vernica <rvernica@gmail.com>
2016-07-02 21:44:05 -0700

    Trap signal and shut down scidb and postgresql

--- scidb/15.7/docker-entrypoint.sh
+++ scidb/15.7/docker-entrypoint.sh
@@ -10,6 +10,11 @@ $SCIDB_INSTALL_PATH/bin/scidb.py startall $SCIDB_NAME
 /etc/init.d/shimsvc start
 
 
+trap "$SCIDB_INSTALL_PATH/bin/scidb.py stopall $SCIDB_NAME; \
+      service postgresql stop" \
+     EXIT HUP INT QUIT TERM
+
+
 if [ "$1" = '' ]
 then
     tail -f $SCIDB_DATA_PATH/000/0/scidb.log
