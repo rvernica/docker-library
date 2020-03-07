f82cb46974897099ed1b64551eedfda5a1b7d75f
Rares Vernica <rvernica@gmail.com>
2020-03-07 15:26:30 -0800

    Dockerfile for SciDB 19.11 based on CentOS 7 WIP #11

---
+++ scidb/19.11/docker-entrypoint-centos-7.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -o errexit
+
+if [ -z "$SCIDB_DATA_PATH" ]
+then
+    SCIDB_DATA_PATH=$SCIDB_INSTALL_PATH/DB-$SCIDB_NAME
+fi
+
+
+echo "----------------------"
+echo "Start Docker container with the following arguments:"
+echo "  --volume /sys/fs/cgroup:/sys/fs/cgroup:ro"
+echo '  --volume /tmp/$(mktemp --directory):/run'
+echo "Start SciDB by running"
+echo "  $SCIDB_INSTALL_PATH/bin/scidb-ctl.py start $SCIDB_NAME"
+echo "----------------------"
+
+
+trap "$SCIDB_INSTALL_PATH/bin/scidb.py stopall $SCIDB_NAME"     \
+     EXIT HUP INT QUIT TERM
+
+
+exec /usr/sbin/init
