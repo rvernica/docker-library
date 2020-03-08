f8f2e5ecd63ca1f2825c0430e57e87091aaabb2f
Rares Vernica <rvernica@gmail.com>
2020-03-07 16:24:31 -0800

    Fix typo and cleanup

--- scidb/19.3/docker-entrypoint-centos-7.sh
+++ scidb/19.3/docker-entrypoint-centos-7.sh
@@ -12,7 +12,7 @@ echo "Start Docker container with the following arguments:"
 echo "  --volume /sys/fs/cgroup:/sys/fs/cgroup:ro"
 echo '  --volume /tmp/$(mktemp --directory):/run'
 echo "Start SciDB by running"
-echo "  $SCIDB_INSTALL_PATH/bin/scidb-ctl.py start $SCIDB_NAME"
+echo "  $SCIDB_INSTALL_PATH/bin/scidbctl.py start $SCIDB_NAME"
 echo "----------------------"
 
 
