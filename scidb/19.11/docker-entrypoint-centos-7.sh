ba0d64a1654b016e779ef27895716d3f5fbe85e6
Rares Vernica <rvernica@gmail.com>
2020-05-07 22:58:07 -0700

    Add back SSL development library (needed by Shim)
    * Add notes on starting shim on CentOS-7

--- scidb/19.11/docker-entrypoint-centos-7.sh
+++ scidb/19.11/docker-entrypoint-centos-7.sh
@@ -13,6 +13,8 @@ echo "  --volume /sys/fs/cgroup:/sys/fs/cgroup:ro"
 echo '  --volume /tmp/$(mktemp --directory):/run'
 echo "Start SciDB by running"
 echo "  $SCIDB_INSTALL_PATH/bin/scidbctl.py start $SCIDB_NAME"
+echo "Start Shim by running"
+echo "  systemctl start shimsvc"
 echo "----------------------"
 
 
